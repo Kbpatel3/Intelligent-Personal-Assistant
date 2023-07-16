cb9e045872ab15307349767335929e9d1d54c1ed
kbpatel <kbpatel3@catamount.wcu.edu>
2023-07-16 10:53:48 -0400

    Refactored use of speech recognizer package

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import speech_recognition as sr
 import pyttsx3 as tts
 from assistants import weather, time, joke, news, wikipedia, web_search
 from utils import nlp
+from utils import speech_recognition as srutils
+import speech_recognition as sr
 
 
 def process_command(command):
@@ -32,29 +33,18 @@ def process_command(command):
 
 
 def main():
-    r = sr.Recognizer()
     while True:
         try:
-            with sr.Microphone() as mic:
-                r.adjust_for_ambient_noise(mic, duration=1.0)
-                print('Listening...')
-                audio = r.listen(mic, timeout=5, phrase_time_limit=5)
-                print("Processing...")
-                text = r.recognize_google(audio)
-                text = text.lower()
-                print(f'You said: {text}')
-                process_command(text)
+            speech_text = srutils.recognize()
+            process_command(speech_text)
         except sr.UnknownValueError:
             print('Sorry, I did not get that')
-            r = sr.Recognizer()
             continue
         except sr.RequestError as e:
             print(f'Sorry, my speech service is down {e}')
-            r = sr.Recognizer()
             continue
         except sr.WaitTimeoutError:
             print("Timeout: No speech detected")
-            r = sr.Recognizer()
             continue
 
 
